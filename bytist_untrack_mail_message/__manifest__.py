ba63edb5689f7a6ecd2244104bb3128d43043adc
Florian Bieringer <github@fl0.eu>
2025-03-12 21:44:00 +0100

    Add untrack mail message

---
+++ bytist_untrack_mail_message/__manifest__.py
@@ -0,0 +1,17 @@
+# pylint: disable=pointless-statement
+{
+    'name': "Untrack Mail Message",
+    'summary': """Removes images from mail messages to avoid tracking""",
+    'author': "Bytist GmbH",
+    'website': "https://bytist.de",
+    'category': 'Website',
+    'version': '16.0.1.0.0',
+    'depends': ['mail'],
+    'license': 'OPL-1',
+    'support': 'contact@bytist.de',
+    'data': [
+    ],
+    'images': [
+        'static/description/banner.png'
+    ]
+}
---
+++ bytist_untrack_mail_message/models/mail_message.py
@@ -0,0 +1,13 @@
+import re
+from markupsafe import Markup
+from odoo import models
+
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'
+
+    def _message_format(self, fnames, format_reply=True, legacy=False):
+        vals_list = super()._message_format(fnames=fnames, format_reply=format_reply, legacy=legacy)
+        for msg in vals_list:
+            msg['body'] = Markup(re.sub(r"\s+src\s*=\s*(.*)\s+", r" data-src=\1 ", str(msg['body'])))
+        return vals_list
