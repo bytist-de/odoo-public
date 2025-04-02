abf15493f71ffbf14910b8ed3a34da7e05390653
Florian Bieringer <github@fl0.eu>
2025-04-02 21:46:23 +0200

    Add removed image

--- bytist_untrack_mail_message/models/mail_message.py
+++ bytist_untrack_mail_message/models/mail_message.py
@@ -9,5 +9,5 @@ class MailMessage(models.Model):
     def _message_format(self, fnames, format_reply=True, legacy=False):
         vals_list = super()._message_format(fnames=fnames, format_reply=format_reply, legacy=legacy)
         for msg in vals_list:
-            msg['body'] = Markup(re.sub(r"\s+src\s*=\s*(.*)\s+", r" data-src=\1 ", str(msg['body'])))
+            msg['body'] = Markup(re.sub(r"\s+src\s*=\s*(.*)\s+", r" src='/bytist_untrack_mail_message/static/img/gallery-remove-svgrepo-com.svg' data-src=\1 ", str(msg['body'])))
         return vals_list
