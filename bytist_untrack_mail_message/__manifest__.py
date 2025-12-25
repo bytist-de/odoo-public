db03d49e32c8fedd08c3692092c66cc29c2da7e7
Florian Bieringer <github@fl0.eu>
2025-12-25 11:32:59 +0100

    Update for Odoo 18

--- bytist_untrack_mail_message/__manifest__.py
+++ bytist_untrack_mail_message/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Bytist GmbH",
     'website': "https://bytist.de",
     'category': 'Website',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'depends': ['mail'],
     'license': 'OPL-1',
     'support': 'contact@bytist.de',
--- bytist_untrack_mail_message/models/mail_message.py
+++ bytist_untrack_mail_message/models/mail_message.py
@@ -1,13 +1,13 @@
 import re
 from markupsafe import Markup
 from odoo import models
-
+from odoo.addons.mail.tools.discuss import Store
 
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
-    def _message_format(self, fnames, format_reply=True):
-        vals_list = super()._message_format(fnames=fnames, format_reply=format_reply)
-        for msg in vals_list:
+    def _to_store(self, store: Store, **kwargs):
+        """Extends the channel header by adding the livechat operator and the 'anonymous' profile"""
+        super()._to_store(store, **kwargs)
+        for msg in store.data['mail.message'].values():
             msg['body'] = Markup(re.sub(r"\s+src\s*=\s*(.*)\s+", r" src='/bytist_untrack_mail_message/static/img/gallery-remove-svgrepo-com.svg' data-src=\1 ", str(msg['body'])))
-        return vals_list
