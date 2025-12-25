1753a87ebb86e77ade65377615a9535409721f1e
Florian Bieringer <github@fl0.eu>
2025-12-25 10:25:56 +0100

    Update for version 17.0

--- bytist_untrack_mail_message/__manifest__.py
+++ bytist_untrack_mail_message/__manifest__.py
@@ -5,12 +5,15 @@
     'author': "Bytist GmbH",
     'website': "https://bytist.de",
     'category': 'Website',
-    'version': '16.0.1.0.0',
+    'version': '17.0.1.0.0',
     'depends': ['mail'],
     'license': 'OPL-1',
     'support': 'contact@bytist.de',
     'data': [
     ],
+        'demo': [
+            'demo/demo.xml',
+        ],
     'images': [
         'static/description/banner.png'
     ]
--- bytist_untrack_mail_message/models/mail_message.py
+++ bytist_untrack_mail_message/models/mail_message.py
@@ -6,8 +6,8 @@ from odoo import models
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
-    def _message_format(self, fnames, format_reply=True, legacy=False):
-        vals_list = super()._message_format(fnames=fnames, format_reply=format_reply, legacy=legacy)
+    def _message_format(self, fnames, format_reply=True):
+        vals_list = super()._message_format(fnames=fnames, format_reply=format_reply)
         for msg in vals_list:
             msg['body'] = Markup(re.sub(r"\s+src\s*=\s*(.*)\s+", r" src='/bytist_untrack_mail_message/static/img/gallery-remove-svgrepo-com.svg' data-src=\1 ", str(msg['body'])))
         return vals_list
