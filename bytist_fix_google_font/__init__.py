8eda8174af3a12bbfceeb7d95bf5f7ae1fd5d2f0
Florian Bieringer <github@fl0.eu>
2025-12-25 09:00:29 +0100

    Remove google fonts plugin

--- bytist_fix_google_font/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import controllers
-from . import models
-from .hooks import uninstall_hook, post_init_hook
--- bytist_fix_google_font/hooks.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, SUPERUSER_ID
-
-def uninstall_hook(cr, registry):
-   env = api.Environment(cr, SUPERUSER_ID, {})
-   env['ir.attachment'].search([('name', 'ilike', 'web.assets_')]).unlink()
-
-def post_init_hook(cr, registry):
-   env = api.Environment(cr, SUPERUSER_ID, {})
-   env['ir.attachment'].search([('name', 'ilike', 'web.assets_')]).unlink()
